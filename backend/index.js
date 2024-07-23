5aa84d1ec33d008d286212b9549c6d02aecef535
AbhijitSingh08 <abhijeet.singh008@gmail.com>
2024-07-23 11:58:57 +0530

    DF edit

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,14 @@ const express = require('express');
 const app = express();
 const port = 3001; // Port number for the server
 
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back index.html.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World! This is our API.');
 });
